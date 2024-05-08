bb3564a4beae540ef91f021119655851d9b3e14f
Fabricio25102002 <162696015+Fabricio25102002@users.noreply.github.com>
2024-05-08 17:50:43 -0300

    Add files via upload

---
+++ c++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main () {
+    printf("insira");
+	float v1,v2,v3,resultado;
+	cin>>v1;
+	   printf("insira");
+	cin>>v2;
+	  printf("insira");
+	cin>>v3; 
+resultado=(v1+v2+v3);
+	cout<<resultado;
+	return 0;
+
+}
