2469f04d36733be6aad0cd0c0992a9dc9f36d2a2
Fabricio25102002 <162696015+Fabricio25102002@users.noreply.github.com>
2024-06-25 21:14:42 -0300

    Add files via upload

---
+++ soma de dois numeros( c++) - Copia.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    int num1, num2, soma;
+
+    std::cout << "Digite o primeiro número: ";
+    std::cin >> num1;
+
+    std::cout << "Digite o segundo número: ";
+    std::cin >> num2;
+
+    soma = num1 + num2;
+
+    std::cout << "A soma dos dois números é: " << soma << std::endl;
+
+    return 0;
