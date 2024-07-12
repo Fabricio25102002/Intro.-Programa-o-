4822f7a082414e4ecf444a555ebd351ad07406f7
Fabricio25102002 <162696015+Fabricio25102002@users.noreply.github.com>
2024-07-11 22:53:52 -0300

    media ponderada c++
    media ponderada c++

---
+++ prova q2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+	float n1,n2,n3,m;
+	cout<< ("digite as medias");
+	
+	cin>>n1,n2,n3;
+	m=n1*0.1+ n2*0.6+ n3*0.3;
+	cout<<"m="<<m;
+	
+	
+		return 0;
+}
