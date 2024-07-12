f402a4f2cd8e5e5edea0805de0bea253dbe62244
Fabricio25102002 <162696015+Fabricio25102002@users.noreply.github.com>
2024-07-11 22:37:08 -0300

    convercao de temperatura c++
    convercao de temperatura c++

---
+++ prova q1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+   float c,f;
+	cout<< ("digite a temperatura em celsius")<<endl;
+	cin>> c;
+	f= c*1.8+32;
+	cout<<("f")<<f;
+	
+	
+	
+	
+	return 0;
+}
