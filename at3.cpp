f8b49844571c4985133cf5379b2f21c4d67d379a
Fabricio25102002 <162696015+Fabricio25102002@users.noreply.github.com>
2024-08-01 15:40:54 -0300

    array

---
+++ at3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, char** argv){
+int soma;
+soma=0;
+	int a [10] = {23,45,45,67,23,12};
+
+	
+	for (int x=0; x<10; x++) { soma=soma+a [x];
+	 
+
+	cout<<"soma" << x<<endl;
+	
+	}	
+	
+	
+	
+	
+	
+	return 0;
+}
