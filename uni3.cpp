3012568986b78f2400a3798df15ebc09a1277292
Fabricio25102002 <162696015+Fabricio25102002@users.noreply.github.com>
2024-08-01 15:40:09 -0300

    c++ array

---
+++ uni3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, char** argv){
+int soma;
+soma=0
+	int a [10];
+
+	
+	for (int x=0; x<10; x++) { soma=soma+a [x]
+	 cout"soma"<<soma;
+
+	
+	
+	}	
+	
+	
+	
+	
+	
+	return 0;
+}
