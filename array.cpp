352595c172a5542070c6e76b449a2a816c34d6a7
Fabricio25102002 <162696015+Fabricio25102002@users.noreply.github.com>
2024-08-01 16:12:26 -0300

    array

---
+++ array.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+	int a [ 5];
+	a[ 0]=10;
+	a[ 1]=30;
+	a[ 4]=60;
+	cout<< a[ 4];
+	return 0;
+}
