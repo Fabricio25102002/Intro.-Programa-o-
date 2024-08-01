cdacb90417a4fcac84f7a5572e33ef232411e334
Fabricio25102002 <162696015+Fabricio25102002@users.noreply.github.com>
2024-08-01 15:41:16 -0300

    c++ array

---
+++ at4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, char** argv){
+int soma;
+int valor=5;
+soma=0;
+	int a [5] = {23,45,45,67,23,12};
+
+	
+	for (int x=0; x<5; x++) { 
+	 
+
+	cout<<"soma" << x<<endl;
+	
+	}	
+	
+	
+	
+	
+	
+	return 0;
+}
